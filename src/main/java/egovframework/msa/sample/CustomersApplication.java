5be1ab07b43d87d968543562674f96cc5d2ac7f0
kimjaeyeol <kjy@solutis.co.kr>
2021-03-01 18:31:19 +0900

    eureka client 적용 - 서비스 시작시  eureka 서버에 자신의 정보를 등록

--- src/main/java/egovframework/msa/sample/CustomersApplication.java
+++ src/main/java/egovframework/msa/sample/CustomersApplication.java
@@ -2,9 +2,11 @@ package egovframework.msa.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("egovframework.*")
+@EnableEurekaClient // eureka client
 @SpringBootApplication
 public class CustomersApplication {
 
